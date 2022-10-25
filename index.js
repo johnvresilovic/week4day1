c5d3d35439af4d87ef3bf5514438669594a392cb
John Vresilovic <johnvresilovic@protonmail.com>
2022-10-25 13:50:06 -0400

    Wild Wild Life answered

--- index.js
+++ index.js
@@ -10,21 +10,36 @@
 // }
 // logEvenNumbers()
 
-const fizzBuzz = () => {
-    for (i = 1; i < 101; i++) {
-        if (i % 3 === 0 && i % 5 === 0) {
-            console.log('FizzBuzz')
-        } else {
-            if (i % 5 === 0) {
-                console.log('Buzz')
-                } else {
-                    if (i % 3 === 0) {
-                        console.log('Fizz')
-                    } else {
-                        console.log(i)
-                    }
-                }
-            }
-        }
-    }
-fizzBuzz()
+// const fizzBuzz = () => {
+//     for (i = 1; i < 101; i++) {
+//         if (i % 3 === 0 && i % 5 === 0) {
+//             console.log('FizzBuzz')
+//         } else {
+//             if (i % 5 === 0) {
+//                 console.log('Buzz')
+//                 } else {
+//                     if (i % 3 === 0) {
+//                         console.log('Fizz')
+//                     } else {
+//                         console.log(i)
+//                     }
+//                 }
+//             }
+//         }
+//     }
+// fizzBuzz()
+
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+plantee[2] +=1
+wolfy[3] = 'Gotham City'
+dart.push('Hawkins')
+wolfy.shift()
+wolfy.unshift('Gameboy')
+console.log(plantee)
+console.log(wolfy)
+console.log(dart)
+console.log(wolfy)
