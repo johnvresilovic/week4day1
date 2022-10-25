d01077c169c654a33e85b9be0eb1243bad140466
John Vresilovic <johnvresilovic@protonmail.com>
2022-10-25 13:31:04 -0400

    Add second function

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-const logNumbers = () => {for (i = 1; i < 21; i++) {console.log(i)}}
-logNumbers()
+// const logNumbers = () => {for (i = 1; i < 21; i++) {console.log(i)}}
+// logNumbers()
+
+// const logEvenNumbers = () => {
+//     for (i = 1; i < 201; i++) {
+//         if (i % 2 === 0) {
+//             console.log(i)
+//         }
+//     }
+// }
+// logEvenNumbers()
+
